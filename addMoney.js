c5c83c70b3140a01e10b9fbdc46bd6540c9c9cb0
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2025-04-27 22:44:06 +0600

    add money and cashout added

--- addMoney.js
+++ addMoney.js
@@ -1,16 +1,26 @@
-document.getElementById('add-money').addEventListener('click',function(event){
+document.getElementById('add-money').addEventListener('click', function (event) {
     event.preventDefault();
-     const amount = document.getElementById('amount').value;
+    const amount = document.getElementById('amount').value;
     //  console.log(typeof amount);
-    const convertAmount =parseFloat(amount);
+    const convertAmount = parseFloat(amount);
 
     const pin = document.getElementById('pin').value;
     const convertPin = parseInt(pin);
 
-    if(convertPin === 1234){
-        console.log('your pin is valid');
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertMainBalanace = parseFloat(mainBalance);
+    console.log(convertMainBalanace);
+
+
+
+
+
+    if (convertPin === 1234) {
+        // console.log('your pin is valid');
+        const sum = convertMainBalanace + convertAmount;
+        document.getElementById('main-balance').innerText = sum;
     }
-    else{
+    else {
         alert('your needed valid pin');
     }
 
---
+++ test/test.js
@@ -0,0 +1,9 @@
+document.getElementById('bangladesh-btn').addEventListener('click', function () {
+    document.getElementById('bd').style.display = 'block';
+    document.getElementById('usa').style.display = 'none';
+})
+
+document.getElementById('usea-btn').addEventListener('click', function () {
+    document.getElementById('bd').style.display = 'none';
+    document.getElementById('usa').style.display = 'block';
+})
